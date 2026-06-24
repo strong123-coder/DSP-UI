25732ddba6f2f5d11e1c2bbbe6887603bb0d498b
Sarthak Sharma <sarthaks225@gmail.com>
2026-06-24 19:40:55 +0530

    changes

--- src/pages/management/users/usersList/components/users-table-body.tsx
+++ src/pages/management/users/usersList/components/users-table-body.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { MoreVertical } from "lucide-react";
 import { TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { Badge } from "@/components/ui/badge";
